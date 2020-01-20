639eec6b539a185d454123dca4f0091bcece8f62
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-19 22:01:27 -0800

    Remove old index.js file stuff into tests for better utility

--- test.js
+++ test.js
@@ -41,3 +41,23 @@ testCases.forEach(testCase => {
     expect(result).toBe(expectedResult);
   });
 });
+
+const wabt = require("wabt")();
+
+// An example file that executes Wasm. Useful for trying Wasm code out.
+// It's in this test so that I can run it in a fast-feedback loop (`jest --watch`)
+test('Can execute Wasm', async () => {
+  const wat = `(module
+      (func (result i32)
+          i32.const 10
+      )
+      (export "run" (func 0))
+  )`;
+  const wasmModule = wabt.parseWat("somefile.wat", wat);
+  const { buffer } = wasmModule.toBinary({});
+  const mod = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(mod);
+  const result = instance.exports.run();
+  expect(result).toBe(10);
+
+})
