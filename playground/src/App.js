0e7ef3dbd874356b6e20dec80603e344173a5de1
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-22 15:26:30 -0800

    Move all hooks to hooks file

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import "./App.css";
-import { parse } from "parser";
-import { emit } from "emitter";
 import Editor, { ControlledEditor } from "@monaco-editor/react";
-import { useUrlState, useForceUpdate } from "./hooks";
-import shims from "shims";
-import _wabt from "wabt";
+import { useUrlState, useForceUpdate, useGlobals, useAst, useWasm, useMod } from "./hooks";
 
-const wabt = _wabt();
 function Column({ children }) {
   return (
     <div
@@ -32,128 +27,10 @@ const EDITOR_OPTIONS = {
   lineNumbers: "off"
 };
 
-async function modFromWat(wat, globals) {
-  if (wat == null || globals == null) {
-    return null;
-  }
 
-  const wasmModule = wabt.parseWat("somefile.wat", wat);
-  const { buffer } = wasmModule.toBinary({});
-  const mod = await WebAssembly.compile(buffer);
-
-  var importObject = {
-    js: { ...globals },
-    imports: shims
-  };
-
-  return await WebAssembly.instantiate(mod, importObject);
-}
-
-function useAst(eel) {
-  const [ast, setAst] = useState(null);
-  const [astError, setAstError] = useState(null);
-  useEffect(() => {
-    try {
-      setAst(parse(eel));
-      setAstError(null);
-    } catch (e) {
-      setAstError(e.message);
-    }
-  }, [eel]);
-
-  return [ast, astError];
-}
-
-function useWasm(ast, globals) {
-  const [wasm, setWasm] = useState(null);
-  const [wasmError, setWasmError] = useState(null);
-
-  useEffect(() => {
-    if (ast == null) {
-      return;
-    }
-    try {
-      const exportedFunctions = [
-        {
-          type: "FUNCTION_EXPORT",
-          name: "main",
-          function: ast
-        }
-      ];
-
-      const moduleAst = { type: "MODULE", exportedFunctions };
-      setWasm(emit(moduleAst, { globals: new Set(Object.keys(globals)) }));
-      setWasmError(null);
-    } catch (e) {
-      setWasmError(e.message);
-    }
-  }, [ast, globals]);
-
-  return [wasm, wasmError];
-}
-
-function useMod(wasm, globals) {
-  const [mod, setMod] = useState(null);
-
-  useEffect(() => {
-    setMod(null);
-    if (wasm == null) {
-      return;
-    }
-    let unmounted = false;
-
-    modFromWat(wasm, globals).then(mod => {
-      if (unmounted) {
-        return;
-      }
-      setMod(mod);
-    });
-
-    return () => {
-      unmounted = true;
-    };
-    // We intentionally omit `globals` from this array. When globals updates it
-    // should generate a new `wasm` string, and we want to ensure we wait for the
-    // new `wasm` rather than trying to build the mod with the old `wasm` and the
-    // new `globals`.
-  }, [wasm]);
-
-  return mod;
-}
-
-function serializeGlobals(globals) {
-  const obj = {};
-  Object.entries(globals).forEach(([name, global]) => {
-    obj[name] = global.value;
-  });
-  return JSON.stringify(obj);
-}
-
-function deserializeGlobals(str) {
-  if (!str) {
-    return {};
-  }
-  const globals = {};
-  try {
-    const obj = JSON.parse(str);
-    Object.entries(obj).forEach(([name, value]) => {
-      globals[name] = new WebAssembly.Global(
-        { value: "f64", mutable: true },
-        value
-      );
-    });
-  } catch (e) {
-    console.error(e);
-  }
-  return globals;
-}
 
 function App() {
-  const [globals, setGlobals] = useUrlState(
-    "globals",
-    {},
-    { serialize: serializeGlobals, deserialize: deserializeGlobals }
-  );
+  const { globals, addGlobal, removeGlobal } = useGlobals();
   // const [globals, setGlobals] = useState({});
   const [eel, setEel] = useUrlState("eel", "foo = 1;");
   const [astString, setAstString] = useState(null);
@@ -171,28 +48,11 @@ function App() {
       mod.exports.main();
       forceUpdate();
     };
-  }, [mod]);
+  }, [forceUpdate, mod]);
 
   useEffect(() => {
     setAstString(JSON.stringify(ast, null, 2));
   }, [ast]);
-
-  const addGlobal = useCallback(name => {
-    setGlobals(globals => {
-      return {
-        ...globals,
-        [name]: new WebAssembly.Global({ value: "f64", mutable: true }, 0)
-      };
-    });
-  }, []);
-
-  // TODO: This currently crashes because we try to create the mod tries to update before the new wasm gets generated.
-  const removeGlobal = useCallback(name => {
-    setGlobals(globals => {
-      const { [name]: _, ...newGlobals } = globals;
-      return newGlobals;
-    });
-  }, []);
 
   return (
     <div style={{ display: "flex", width: "100vw", alignContent: "stretch" }}>
@@ -221,7 +81,7 @@ function App() {
                   forceUpdate();
                 }}
               />
-              <button onClick={() => removeGlobal(name)}>-</button>
+              <button onClick={() => removeGlobal(name)}>X</button>
             </label>
           );
         })}
--- playground/src/hooks.js
+++ playground/src/hooks.js
@@ -1,4 +1,10 @@
 import { useState, useEffect, useCallback } from "react";
+import { parse } from "parser";
+import { emit } from "emitter";
+import shims from "shims";
+import _wabt from "wabt";
+
+const wabt = _wabt();
 
 const identity = val => val;
 
@@ -15,7 +21,7 @@ export function useUrlState(key, initial, {serialize = identity, deserialize = i
     const newRelativePathQuery =
       window.location.pathname + "?" + params.toString();
     window.history.pushState(null, "", newRelativePathQuery);
-  }, [eel, key]);
+  }, [eel, key, serialize]);
   return [eel, setEel];
 }
 
@@ -25,4 +31,146 @@ export function useForceUpdate() {
   return useCallback(() => {
     setRunCount(count => count + 1);
   }, []);
+}
+
+async function modFromWat(wat, globals) {
+  if (wat == null || globals == null) {
+    return null;
+  }
+
+  const wasmModule = wabt.parseWat("somefile.wat", wat);
+  const { buffer } = wasmModule.toBinary({});
+  const mod = await WebAssembly.compile(buffer);
+
+  var importObject = {
+    js: { ...globals },
+    imports: shims
+  };
+
+  return await WebAssembly.instantiate(mod, importObject);
+}
+
+export function useAst(eel) {
+  const [ast, setAst] = useState(null);
+  const [astError, setAstError] = useState(null);
+  useEffect(() => {
+    try {
+      setAst(parse(eel));
+      setAstError(null);
+    } catch (e) {
+      setAstError(e.message);
+    }
+  }, [eel]);
+
+  return [ast, astError];
+}
+
+export function useWasm(ast, globals) {
+  const [wasm, setWasm] = useState(null);
+  const [wasmError, setWasmError] = useState(null);
+
+  useEffect(() => {
+    if (ast == null) {
+      return;
+    }
+    try {
+      const exportedFunctions = [
+        {
+          type: "FUNCTION_EXPORT",
+          name: "main",
+          function: ast
+        }
+      ];
+
+      const moduleAst = { type: "MODULE", exportedFunctions };
+      setWasm(emit(moduleAst, { globals: new Set(Object.keys(globals)) }));
+      setWasmError(null);
+    } catch (e) {
+      setWasmError(e.message);
+    }
+  }, [ast, globals]);
+
+  return [wasm, wasmError];
+}
+
+export function useMod(wasm, globals) {
+  const [mod, setMod] = useState(null);
+
+  useEffect(() => {
+    setMod(null);
+    if (wasm == null) {
+      return;
+    }
+    let unmounted = false;
+
+    modFromWat(wasm, globals).then(mod => {
+      if (unmounted) {
+        return;
+      }
+      setMod(mod);
+    });
+
+    return () => {
+      unmounted = true;
+    };
+    // We intentionally omit `globals` from this array. When globals updates it
+    // should generate a new `wasm` string, and we want to ensure we wait for the
+    // new `wasm` rather than trying to build the mod with the old `wasm` and the
+    // new `globals`.
+  }, [globals, wasm]);
+
+  return mod;
+}
+
+function serializeGlobals(globals) {
+  const obj = {};
+  Object.entries(globals).forEach(([name, global]) => {
+    obj[name] = global.value;
+  });
+  return JSON.stringify(obj);
+}
+
+function deserializeGlobals(str) {
+  if (!str) {
+    return {};
+  }
+  const globals = {};
+  try {
+    const obj = JSON.parse(str);
+    Object.entries(obj).forEach(([name, value]) => {
+      globals[name] = new WebAssembly.Global(
+        { value: "f64", mutable: true },
+        value
+      );
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  return globals;
+}
+
+export function useGlobals() {
+  const [globals, setGlobals] = useUrlState(
+    "globals",
+    {},
+    { serialize: serializeGlobals, deserialize: deserializeGlobals }
+  );
+
+  const addGlobal = useCallback(name => {
+    setGlobals(globals => {
+      return {
+        ...globals,
+        [name]: new WebAssembly.Global({ value: "f64", mutable: true }, 0)
+      };
+    });
+  }, [setGlobals]);
+
+  const removeGlobal = useCallback(name => {
+    setGlobals(globals => {
+      const { [name]: _, ...newGlobals } = globals;
+      return newGlobals;
+    });
+  }, [setGlobals]);
+
+  return { globals, addGlobal, removeGlobal };
 }
