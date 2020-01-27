bbb0ad275fcddcbeeaf4151bed63661f90b3cb6c
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-26 20:58:26 -0800

    Shim in some more stuff that I'm too lazy to write in Wasm right now

--- shims.js
+++ shims.js
@@ -7,6 +7,8 @@ module.exports = {
   acos: Math.acos,
   atan: Math.atan,
   atan2: Math.atan2,
+  rand: (a) => Math.floor(Math.random() * a),
+  sqr: (a) => Math.pow(a, 2),
   pow: Math.pow,
   log: Math.log,
   log10: Math.log10,
