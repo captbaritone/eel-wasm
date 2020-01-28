471c997e9ae1ab31bdd465676b75466597f9a8f4
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-27 22:03:22 -0800

    Move wasm toolbench into tools

--- src/test.js
+++ src/test.js
@@ -166,25 +166,6 @@ describe("Small test cases", () => {
   });
 });
 
-const wabt = require("wabt")();
-
-// An example file that executes Wasm. Useful for trying Wasm code out.
-// It's in this test so that I can run it in a fast-feedback loop (`jest --watch`)
-test("Can execute Wasm", async () => {
-  const wat = `(module
-      (func (result i32)
-          i32.const 10
-      )
-      (export "run" (func 0))
-  )`;
-  const wasmModule = wabt.parseWat("somefile.wat", wat);
-  const { buffer } = wasmModule.toBinary({});
-  const mod = await WebAssembly.compile(buffer);
-  const instance = await WebAssembly.instantiate(mod);
-  const result = instance.exports.run();
-  expect(result).toBe(10);
-});
-
 test("Some actual equations", async () => {
   const globals = {};
   MILKDROP_GLOBALS.forEach(name => {
---
+++ tools/wasm.test.js
@@ -0,0 +1,20 @@
+
+// An example file that executes Wasm. Useful for trying Wasm code out.
+// It's in this test so that I can run it in a fast-feedback loop (`jest --watch`)
+
+const wabt = require("wabt")();
+
+test("Can execute Wasm", async () => {
+  const wat = `(module
+      (func (result i32)
+          i32.const 10
+      )
+      (export "run" (func 0))
+  )`;
+  const wasmModule = wabt.parseWat("somefile.wat", wat);
+  const { buffer } = wasmModule.toBinary({});
+  const mod = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(mod);
+  const result = instance.exports.run();
+  expect(result).toBe(10);
+});
