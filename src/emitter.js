b3e04343135e71523acedeb13b8c0c05c30b28a9
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-29 10:49:42 -0800

    Progress toward emitting arrays of instructions

--- src/emitter.js
+++ src/emitter.js
@@ -12,109 +12,183 @@ function makeNamespaceResolver(prefix) {
   };
 }
 
+const TRY = false;
+
+// These are temporary functions to help get us from wat to wasm
+function cat(arr) {
+  if(TRY) {
+    return arr;
+  }
+  arr.forEach(val => {
+    if (Array.isArray(val)) {
+      throw new Error("Cat an array", val);
+    }
+    if(/\s/.test(val)) {
+      // throw new Error(`Value had a space! "${val}"`)
+    }
+  });
+  return arr.join(" ");
+}
+
+function split(str) {
+  if(TRY) {
+    return str;
+  }
+  return str.split(" ");
+}
+
+function arrayJoin(arr, joiner) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+    const last = i === arr.length - 1;
+    if (!last) {
+      newArr.push(joiner);
+    }
+  }
+  return newArr;
+}
+
+function flatten(arr) {
+  const newArr = [];
+  arr.forEach(subArr => {
+    newArr.push(...subArr);
+  });
+  return newArr;
+}
+
 // TODO: These functions could either be lazily added (only appended when used)
 // or inlined.
 const STANDARD_LIBRARY = `
 ;; TODO: We should double check that this does not short circut
 (func $if (param $test f64) (param $consiquent f64) (param $alternate f64) (result f64) 
-  get_local $consiquent
-  get_local $alternate
-  get_local $test
-  f64.const 0 f64.ne
-  select 
+  ${cat([
+    "get_local",
+    "$consiquent",
+    "get_local",
+    "$alternate",
+    "get_local",
+    "$test",
+    "f64.const",
+    0,
+    "f64.ne",
+    "select"
+  ])}
 )
 ;; TODO: Simplify all this type coersion
 (func $booleanOr (param $a f64) (param $b f64) (result f64) 
-  get_local $a
-  i32.trunc_s/f64
-  get_local $b
-  i32.trunc_s/f64
-  i32.or
-  i32.const 0
-  i32.ne
-  f64.convert_s/i32
+  ${cat([
+    "get_local",
+    "$a",
+    "i32.trunc_s/f64",
+    "get_local",
+    "$b",
+    "i32.trunc_s/f64",
+    "i32.or",
+    "i32.const",
+    0,
+    "i32.ne",
+    "f64.convert_s/i32"
+  ])}
 )
 (func $mod (param $a f64) (param $b f64) (result f64) 
-  get_local $a
-  i64.trunc_s/f64
-  get_local $b
-  i64.trunc_s/f64
-  i64.rem_s
-  f64.convert_s/i64
+  ${cat([
+    "get_local",
+    "$a",
+    "i64.trunc_s/f64",
+    "get_local",
+    "$b",
+    "i64.trunc_s/f64",
+    "i64.rem_s",
+    "f64.convert_s/i64"
+  ])}
 )
 (func $bitwiseAnd (param $a f64) (param $b f64) (result f64) 
-  get_local $a
-  i64.trunc_s/f64
-  get_local $b
-  i64.trunc_s/f64
-  i64.and
-  f64.convert_s/i64
-)
+  ${cat([
+    "get_local",
+    "$a",
+    "i64.trunc_s/f64",
+    "get_local",
+    "$b",
+    "i64.trunc_s/f64",
+    "i64.and",
+    "f64.convert_s/i64"
+  ])})
 (func $bitwiseOr (param $a f64) (param $b f64) (result f64) 
-  get_local $a
-  i64.trunc_s/f64
-  get_local $b
-  i64.trunc_s/f64
-  i64.or
-  f64.convert_s/i64
+  ${cat([
+    "get_local",
+    "$a",
+    "i64.trunc_s/f64",
+    "get_local",
+    "$b",
+    "i64.trunc_s/f64",
+    "i64.or",
+    "f64.convert_s/i64"
+  ])}
 )
 (func $booleanNot (param $x f64) (result f64) 
-  get_local $x
-  i32.trunc_s/f64
-  i32.eqz
-  f64.convert_s/i32
-)
+  ${cat([
+    "get_local",
+    "$x",
+    "i32.trunc_s/f64",
+    "i32.eqz",
+    "f64.convert_s/i32"
+  ])})
 (func $sqr (param $x f64) (result f64) 
-  get_local $x
-  get_local $x
-  f64.mul
+  ${cat(["get_local", "$x", "get_local", "$x", "f64.mul"])}
 )
 (func $sign (param $x f64) (result f64) 
-  f64.const 0
-  get_local $x
-  f64.lt
-
-  get_local $x
-  f64.const 0
-  f64.lt
-
-  i32.sub
-  f64.convert_s/i32
-)
+  ${cat([
+    "f64.const",
+    0,
+    "get_local",
+    "$x",
+    "f64.lt",
+    "get_local",
+    "$x",
+    "f64.const",
+    0,
+    "f64.lt",
+    "i32.sub",
+    "f64.convert_s/i32"
+  ])})
 `;
 
 const BINARY_OPERATORS = {
-  "+": "f64.add",
-  "-": "f64.sub",
-  "*": "f64.mul",
-  "/": "f64.div",
-  "%": "call $mod",
-  "|": "call $bitwiseOr",
-  "&": "call $bitwiseAnd"
+  "+": ["f64.add"],
+  "-": ["f64.sub"],
+  "*": ["f64.mul"],
+  "/": ["f64.div"],
+  "%": ["call", "$mod"],
+  "|": ["call", "$bitwiseOr"],
+  "&": ["call", "$bitwiseAnd"]
 };
 
 const FUNCTIONS = {
-  abs: { arity: 1, instruction: "f64.abs" },
-  sqrt: { arity: 1, instruction: "f64.sqrt" },
-  sqr: { arity: 1, instruction: "call $sqr" },
-  sign: { arity: 1, instruction: "call $sign" },
+  abs: { arity: 1, instruction: ["f64.abs"] },
+  sqrt: { arity: 1, instruction: ["f64.sqrt"] },
+  sqr: { arity: 1, instruction: ["call", "$sqr"] },
+  sign: { arity: 1, instruction: ["call", "$sign"] },
   // TODO: What's the difference between trunc and floor?
   // TODO: Is a rounded float the right thing here, or do we want an int?
-  int: { arity: 1, instruction: "f64.floor" },
-  min: { arity: 2, instruction: "f64.min" },
-  max: { arity: 2, instruction: "f64.max" },
+  int: { arity: 1, instruction: ["f64.floor"] },
+  min: { arity: 2, instruction: ["f64.min"] },
+  max: { arity: 2, instruction: ["f64.max"] },
   // We use `lt` here rather than `gt` because the stack is backwards.
-  above: { arity: 2, instruction: "f64.lt f64.convert_i32_s" },
+  above: { arity: 2, instruction: ["f64.lt", "f64.convert_i32_s"] },
   // We use `gt` here rather than `lt` because the stack is backwards.
-  below: { arity: 2, instruction: "f64.gt f64.convert_i32_s" },
-  equal: { arity: 2, instruction: "f64.eq f64.convert_i32_s" },
-  bnot: { arity: 1, instruction: "call $booleanNot" },
-  bor: { arity: 2, instruction: "call $booleanOr" },
-  if: { arity: 3, instruction: "call $if" }
+  below: { arity: 2, instruction: ["f64.gt", "f64.convert_i32_s"] },
+  equal: { arity: 2, instruction: ["f64.eq", "f64.convert_i32_s"] },
+  bnot: { arity: 1, instruction: ["call", "$booleanNot"] },
+  bor: { arity: 2, instruction: ["call", "$booleanOr"] },
+  if: { arity: 3, instruction: ["call", "$if"] }
 };
 
 Object.entries(shims).forEach(([key, value]) => {
-  FUNCTIONS[key] = { arity: value.length, instruction: `call $${key}` };
+  FUNCTIONS[key] = {
+    arity: value.length,
+    instruction: [`call`, "$" + key]
+  };
 });
 
 function emit(ast, context) {
@@ -152,7 +226,8 @@ function emit(ast, context) {
       )`;
     }
     case "FUNCTION_EXPORT": {
-      const body = emit(ast.function, context);
+      // We do a real join here since from here on out we must have strings
+      const body = split(emit(ast.function, context)).join(' ');
       // TODO: Should functions have implicit return?
       // This could be complex, since programs can be empty (I think).
       return `(func ${context.resolveImport(ast.name)}  ${body})
@@ -160,25 +235,25 @@ function emit(ast, context) {
     }
     case "SCRIPT": {
       const body = ast.body.map((statement, i) => {
-        return `${emit(statement, context)} drop`;
+        return cat([...split(emit(statement, context)), "drop"]);
       });
 
-      return body.join("\n");
+      return cat(arrayJoin(body, "\n"));
     }
     case "EXPRESSION_BLOCK": {
       const body = ast.body.map((statement, i) => {
-        return emit(statement, context);
+        return split(emit(statement, context));
       });
-      return body.join(" drop\n");
+      return cat(flatten(arrayJoin(body, ["drop"])));
     }
     case "BINARY_EXPRESSION": {
-      const left = emit(ast.left, context);
-      const right = emit(ast.right, context);
+      const left = split(emit(ast.left, context));
+      const right = split(emit(ast.right, context));
       const instruction = BINARY_OPERATORS[ast.operator];
       if (instruction == null) {
         throw new Error(`Unknown binary operator ${ast.operator}`);
       }
-      return `${left} ${right} ${instruction}`;
+      return cat([...left, ...right, ...instruction]);
     }
     case "CALL_EXPRESSION": {
       const func = FUNCTIONS[ast.callee.value];
@@ -193,11 +268,13 @@ function emit(ast, context) {
           `Incorrect number of arguments passed to ${ast.callee.value}. Got ${ast.arguments.length}, expected ${arity}`
         );
       }
-      const args = ast.arguments.map(node => emit(node, context));
-      return `${args.join(" ")} ${instruction}`;
+      const args = flatten(ast.arguments.map(node => {
+        return split(emit(node, context))
+      }));
+      return cat([...args, ...instruction]);
     }
     case "ASSIGNMENT_EXPRESSION": {
-      const right = emit(ast.right, context);
+      const right = split(emit(ast.right, context));
       const variableName = ast.left.value;
       const global = context.globals.has(variableName);
 
@@ -214,22 +291,22 @@ function emit(ast, context) {
       // cases we should try to find a way to omit the `get/drop` combo.
       // Peephole optimization seems to be the conventional way to do this.
       // https://en.wikipedia.org/wiki/Peephole_optimization
-      const get = `global.get ${resolvedName}`;
-      const set = `global.set ${resolvedName}`;
+      const get = [`global.get`, resolvedName];
+      const set = [`global.set`, resolvedName];
 
       switch (ast.operator) {
         case "=":
-          return `${right} ${set} ${get}`;
+          return cat([...right, ...set, ...get]);
         case "+=":
-          return `${get} ${right} f64.add ${set} ${get}`;
+          return cat([...get, ...right, "f64.add", ...set, ...get]);
         case "-=":
-          return `${get} ${right} f64.sub ${set} ${get}`;
+          return cat([...get, ...right, "f64.sub", ...set, ...get]);
         case "*=":
-          return `${get} ${right} f64.mul ${set} ${get}`;
+          return cat([...get, ...right, "f64.mul", ...set, ...get]);
         case "/=":
-          return `${get} ${right} f64.div ${set} ${get}`;
+          return cat([...get, ...right, "f64.div", ...set, ...get]);
         case "%=":
-          return `${get} ${right} call $mod ${set} ${get}`;
+          return cat([...get, ...right, "call", "$mod", ...set, ...get]);
         default:
           throw new Error(`Unknown assignment operator "${ast.operator}"`);
       }
@@ -251,12 +328,12 @@ function emit(ast, context) {
       throw new Error("Logical expressions are not implemented yet.");
     }
     case "UNARY_EXPRESSION": {
-      const value = emit(ast.value, context);
+      const value = split(emit(ast.value, context));
       switch (ast.operator) {
         case "-":
-          return `${value} f64.neg`;
+          return cat([...value, `f64.neg`]);
         case "+":
-          return value;
+          return cat([...value]);
         default:
           throw new Error(`Unknown unary operator ${ast.operator}`);
       }
@@ -267,16 +344,16 @@ function emit(ast, context) {
         // TODO: It's a bit odd that not every IDENTIFIER node gets emitted. In
         // function calls and assignments we just peek at the name and never emit
         // it.
-        return `global.get ${context.resolveExternalVar(variableName)}`;
+        return cat([`global.get`, context.resolveExternalVar(variableName)]);
       }
       if (!context.userVars.has(variableName)) {
         // EEL lets you access variables before you define them, so we register
         // each access that we encounter.
         context.userVars.add(variableName);
       }
-      return `global.get ${context.resolveUserVar(variableName)}`;
+      return cat([`global.get`, context.resolveUserVar(variableName)]);
     case "NUMBER_LITERAL":
-      return `f64.const ${ast.value}`;
+      return cat([`f64.const`, ast.value]);
     default:
       throw new Error(`Unknown AST node type ${ast.type}`);
   }
