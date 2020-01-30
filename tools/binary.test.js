f217c3ba559bdebd2a2d0c79c4f3e82503b86e8c
Jordan Eldredge <jordan@jordaneldredge.com>
2020-01-30 06:44:30 -0800

    Emit the global section in binary

--- tools/binary.test.js
+++ tools/binary.test.js
@@ -22,6 +22,7 @@ const ieee754 = require("ieee754");
 const SECTION = {
   TYPE: 1,
   FUNC: 3,
+  GLOBAL: 6,
   EXPORT: 7,
   CODE: 10
 };
@@ -43,6 +44,11 @@ const VAL_TYPE = {
   i64: 0x7e,
   f32: 0x7d,
   f64: 0x7c
+};
+
+const MUTABILITY = {
+  const: 0x00,
+  var: 0x01
 };
 
 // http://webassembly.github.io/spec/core/binary/types.html#function-types
@@ -125,44 +131,72 @@ test("Can execute hand crafted binary Wasm", async () => {
   ];
 
   // https://webassembly.github.io/spec/core/binary/modules.html#function-section
+  // This makes no sense. It should be a vector of length one containing a single value 0.
   const funcSection = [
     SECTION.FUNC,
     0x02, // Vector length?
     0x01,
-    0x00,
-    0x07 // Could this actually be the secion type for export?
+    0x00
+    // 0x07 // Could this actually be the secion type for export?
   ];
 
-  const runExport = [...encodeString("run"), EXPORT_TYPE.FUNC, 0x00];
+  const gGlobal = [
+    // globalType
+    0x01, // WAT?
+    VAL_TYPE.f64,
+    MUTABILITY.var,
+    OPS.f64_const,
+    ...encodeNumber(0),
+    OPS.end
+  ];
+
+  const globalSection = [SECTION.GLOBAL, ...encodeVector([...gGlobal])];
+
+  const runFunctionFunctionIndex = 0; // Offset
+  const runFunctionExportDescription = EXPORT_TYPE.FUNC;
+
+  const runExport = [
+    ...encodeString("run"),
+    runFunctionExportDescription,
+    runFunctionFunctionIndex
+  ];
 
   // https://webassembly.github.io/spec/core/binary/modules.html#binary-exportsec
   const exportSection = [SECTION.EXPORT, ...encodeVector([runExport])];
 
+  // This outer vector adds the "size" of the code.
+  //
+  // "Like with sections, the code size is not needed for decoding, but can be
+  // used to skip functions when navigating through a binary. The module is
+  // malformed if a size does not match the length of the respective function
+  // code."
+  const runCode = encodeVector([
+    // vector of locals
+    ...encodeVector([]),
+    // Code proper
+    OPS.f64_const,
+    ...encodeNumber(10),
+    // End
+    OPS.end
+  ]);
+  const codes = encodeVector([runCode]);
+
   // https://webassembly.github.io/spec/core/binary/modules.html#code-section
-  const codeSection = [
-    SECTION.CODE,
-    0x0d, // WAT?
-    ...encodeVector([
-      encodeVector([
-        // vector of locals
-        ...encodeVector([]),
-        // Code proper
-        ...[OPS.f64_const, ...encodeNumber(10)],
-        OPS.end
-      ])
-    ])
-  ];
+  const codeSection = [SECTION.CODE, ...encodeVector(codes)];
 
   const buffer = new Uint8Array([
     ...magicModuleHeader,
     ...moduleVersion,
     ...typeSection,
     ...funcSection,
+    ...globalSection,
+    0x07, // WAT?
     ...exportSection,
     ...codeSection
   ]);
 
   const wat = `(module
+    (global $U0 (mut f64) f64.const 0)
     (func (result f64)
         f64.const 10
     )
