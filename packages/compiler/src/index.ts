3c489aa21c0dea1135ebc73793f094771366be5e
Jordan Eldredge <jordan@jordaneldredge.com>
2020-04-14 20:19:19 -0800

    Don't export the pretty printer
    Without the optimizer, the playground does not need this any more

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,7 +1,6 @@
 import { parse } from "./parser";
 import { compileModule } from "./compiler";
 import shims from "./shims";
-import { print } from "../tools/prettyPrinter";
 import { loadModule } from "./evaluator";
 
-export { parse, compileModule, shims, print, loadModule };
+export { parse, compileModule, shims, loadModule };
