2a8c2caa447a7a397ad94d9f9ef1a7b8a3599e57
Jordan Eldredge <jordan@jordaneldredge.com>
2020-04-17 18:15:57 -0700

    Clean up error formatting code

---
+++ packages/compiler/src/__tests__/errorsUtils.test.ts
@@ -0,0 +1,128 @@
+import fs from "fs";
+import path from "path";
+import { compileModule } from "../compiler";
+import shims from "../shims";
+import { printLoc } from "../errorUtils";
+
+const DIR = path.join(__dirname, "../../fixtures/errors");
+
+const testCases = fs.readdirSync(DIR);
+
+testCases.forEach((filename: string) => {
+  const testFunc = filename.startsWith("only.") ? test.only : test;
+  const testName = filename.replace(/^only\./, "");
+  testFunc(`${testName}`, () => {
+    const eel = fs.readFileSync(path.join(DIR, filename), { encoding: "utf8" });
+    let compilerError = null;
+    try {
+      compileModule({ globals: new Set(), functions: { run: eel }, shims });
+    } catch (e) {
+      compilerError = e;
+    }
+
+    expect(compilerError).not.toBe(null);
+
+    expect(compilerError.message).toMatchSnapshot();
+    // We prepend a newline here so that the error starts on a newline rather
+    // than a '"' in the snapshot and our underline alinment is easy to see.
+    expect("\n" + compilerError.sourceContext).toMatchSnapshot();
+  });
+});
+
+describe("printLoc", () => {
+  test("One line", () => {
+    const frame = printLoc(
+      { first_line: 1, first_column: 6, last_line: 1, last_column: 11 },
+      "Hello there this is a string"
+    );
+    expect("\n" + frame + "\n").toMatchInlineSnapshot(`
+      "
+      > 1 | Hello there this is a string
+          |       ^^^^^
+      "
+    `);
+  });
+  test("One line within multiple lines", () => {
+    const frame = printLoc(
+      { first_line: 4, first_column: 6, last_line: 4, last_column: 11 },
+      [
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+      ].join("\n")
+    );
+    expect("\n" + frame + "\n").toMatchInlineSnapshot(`
+      "
+        3 | Hello there this is a string
+      > 4 | Hello there this is a string
+          |       ^^^^^
+        5 | Hello there this is a string
+      "
+    `);
+  });
+  test("One line within multiple lines + 2 lines of context", () => {
+    const frame = printLoc(
+      { first_line: 4, first_column: 6, last_line: 4, last_column: 11 },
+      [
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+      ].join("\n"),
+      2
+    );
+    expect("\n" + frame + "\n").toMatchInlineSnapshot(`
+      "
+        2 | Hello there this is a string
+        3 | Hello there this is a string
+      > 4 | Hello there this is a string
+          |       ^^^^^
+        5 | Hello there this is a string
+        6 | Hello there this is a string
+      "
+    `);
+  });
+  test("Multiple lines", () => {
+    const frame = printLoc(
+      { first_line: 1, first_column: 6, last_line: 2, last_column: 11 },
+      "Hello there this is a string\nHello there this is a string"
+    );
+    expect("\n" + frame + "\n").toMatchInlineSnapshot(`
+      "
+      > 1 | Hello there this is a string
+      > 2 | Hello there this is a string
+      "
+    `);
+  });
+
+  test("Multiple lines within multiple lines", () => {
+    const frame = printLoc(
+      { first_line: 3, first_column: 6, last_line: 5, last_column: 11 },
+      [
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+        "Hello there this is a string",
+      ].join("\n")
+    );
+    expect("\n" + frame + "\n").toMatchInlineSnapshot(`
+      "
+        2 | Hello there this is a string
+      > 3 | Hello there this is a string
+      > 4 | Hello there this is a string
+      > 5 | Hello there this is a string
+        6 | Hello there this is a string
+      "
+    `);
+  });
+});
--- packages/compiler/src/errorUtils.ts
+++ packages/compiler/src/errorUtils.ts
@@ -4,32 +4,38 @@ function repeat(n: number, char: string) {
   return new Array(n).fill(char).join("");
 }
 
-function printLoc(loc: SourceLocation, rawSource: string, contextLines = 1) {
-  const sourceLines = rawSource.split("\n");
-  const annotatedLines: string[] = [];
-  const singleLineLoc = loc.first_line === loc.last_line;
-  sourceLines.forEach((line, i) => {
-    const lineNumber = i + 1;
-    if (
-      lineNumber < loc.first_line - contextLines ||
-      lineNumber > loc.last_line + contextLines
-    ) {
-      return;
-    }
-    let gutter = " ";
-    if (lineNumber <= loc.first_line && lineNumber >= loc.last_line) {
-      gutter = ">";
-    }
+/* Build up a string showing a formatted source location in context with line numbers.
+ *
+ *   2 | z = sqr(y, 10);
+ * > 3 | x = wat();
+ *     |     ^^^
+ *   4 | y = 100;
+ */
 
-    annotatedLines.push(`${gutter} ${lineNumber} | ${line}`);
+export function printLoc(
+  loc: SourceLocation,
+  rawSource: string,
+  contextLines = 1
+) {
+  const firstIndex = Math.max(loc.first_line - 1 - contextLines, 0);
+  const lastIndex = loc.last_line + contextLines;
 
-    if (singleLineLoc && lineNumber === loc.first_line) {
-      const locLength = loc.last_column - loc.first_column;
-      annotatedLines.push(
-        `    | ${repeat(loc.first_column, " ")}${repeat(locLength, "^")}`
-      );
-    }
+  const sourceLines = rawSource.split("\n").slice(firstIndex, lastIndex);
+
+  const annotatedLines = sourceLines.map((line, i) => {
+    const lineNumber = i + firstIndex + 1;
+    const inRange = lineNumber >= loc.first_line && lineNumber <= loc.last_line;
+    const gutter = inRange ? ">" : " ";
+    return `${gutter} ${lineNumber} | ${line}`;
   });
+
+  if (loc.first_line === loc.last_line) {
+    const padding = repeat(loc.first_column, " ");
+    const underline = repeat(loc.last_column - loc.first_column, "^");
+    const insertIndex = loc.first_line - firstIndex;
+    annotatedLines.splice(insertIndex, 0, `    | ${padding}${underline}`);
+  }
+
   return annotatedLines.join("\n");
 }
 
@@ -39,6 +45,9 @@ class CompilerError extends Error {
 
   constructor(message: string, loc: SourceLocation, rawSource: string) {
     super(message);
+    // TODO: Create an error message that encourages users to open an issue at
+    // https://github.com/captbaritone/eel-wasm/issues is they see this, and gives
+    // Them an easy way to attach the right context.
     this.sourceContext = printLoc(loc, rawSource);
     this.loc = loc;
   }
