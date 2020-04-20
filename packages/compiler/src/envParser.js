200c969204aae4df9617c761ae912fe0dc7bd4cf
Jordan Eldredge <jordan@jordaneldredge.com>
2020-04-19 21:15:45 -0700

    For now always use the compiled parser

--- packages/compiler/src/envParser.js
+++ packages/compiler/src/envParser.js
@@ -1,6 +1,8 @@
 // This module is in charge of selecting either the pre-built module or the one
 // that builds it a runtime with jison.
 
+let parserImpl = require("../build/parser");
+/*
 let parserImpl = null;
 // Always use the dynamic one, while we figure out why the built one doens't work.
 if (process.env.NODE_ENV === "production") {
@@ -8,5 +10,6 @@ if (process.env.NODE_ENV === "production") {
 } else {
   parserImpl = require("../tools/buildParser");
 }
+*/
 
 module.exports = { parse: parserImpl.parse };
